6fa35865190a7b4b544aa0db4faeaf0b89a69460
i-metlyuk <i.metlyuk@mail.ru>
2021-02-22 21:25:35 +0300

    Result
    Nav, accordion, responsive

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,23 @@
-console.log('file 1');
+$(function () {
+    $("#burger").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass('active');
+        $("#nav").toggleClass("active");
+    });
+
+    $("[data-collapse]").on("click", function(event) {
+        event.preventDefault();
+        let $this = $(this);
+        let blockId = $this.data('collapse');
+
+        $this.parent(".accordion__item").toggleClass("active");
+    });
+
+    $("[data-nav]").on("click", function(event){
+        event.preventDefault();
+
+        $("#burger").removeClass("active");
+        $("#nav").removeClass("active");
+    });
+});
